736640c78db4f6327bab1b738c0d98517c99d3c8
creativecreature <hello@creativecreature.com>
2019-05-31 06:02:33 +0200

    ubuntu things

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # dotfiles
-`ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim`
-`ln -sf $HOME/dotfiles/git/gitignore_global $HOME/.gitignore_global`
 run `git config --global core.excludesfile ~/.gitignore_global`
 
 run `chsh -s /bin/zsh` and then restart the computer to change shell to zsh after running the installer
--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -2,13 +2,19 @@
 # ubuntu.sh
 
 
+# -- Hardware settings -----------------------------------------------------------------------
+
+#Add US and SE input sources
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'se')]"
+
+
 
 # -- Git -----------------------------------------------------------------------
 
 if exists "git"; then
   echo_item "Git is already installed" "green"
 else
-  sudo apt-get install git
+  sudo eopkg install git
 fi
 
 echo ""
@@ -21,7 +27,7 @@ if exists "zsh"; then
   echo_item "zsh is already installed" green
 else
   if get_boolean_response "Do you want to install zsh?"; then
-    sudo apt-get install zsh
+    sudo eopkg install zsh
   else
     echo_item "Skipping zsh install" red
   fi
@@ -31,39 +37,20 @@ echo ""
 
 
 
-# -- fzf -----------------------------------------------------------------------
-if exists "fzf"; then
-  echo_item "fzf is already installed" green
+# -- nvm -----------------------------------------------------------------------
+
+if exists "node"; then
+  echo_item "Node tools are already installed" green
 else
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install
-  # TODO: Change vim to use Plug '~/.fzf' instead of Plug '/usr/local/opt/fzf'
-fi
-
-echo ""
-
-
-
-# -- ripgrep ----------------------------------------------------------------------
-if exists "rg"; then
-  echo_item "ripgrep is already installed" green
-else
-  sudo apt-get install ripgrep
-fi
-
-echo ""
-
-
-
-# -- htop ----------------------------------------------------------------------
-
-if exists "htop"; then
-  echo_item "htop is already installed" green
-else
-  if get_boolean_response "Do you want to install htop?"; then
-    sudo apt-get install htop
+  if get_boolean_response "Do you want to install Node.js tools?"; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    nvm install 12.2.0
+    npm install -g yarn
+    npm install -g eslint eslint-plugin-react babel-eslint
   else
-    echo_item "Skipping htop install" red
+    echo_item "Skipping Node.js tools install" red
   fi
 fi
 
@@ -77,11 +64,8 @@ if exists "nvim"; then
   echo_item "Neovim is already installed" green
 else
   if get_boolean_response "Do you want to install Neovim?"; then
-    sudo apt-get --yes --force-yes install software-properties-common
-    sudo add-apt-repository ppa:neovim-ppa/unstable
-    sudo apt-get update
-    sudo apt-get --yes --force-yes install neovim python-dev python-pip python3-dev python3-pip
-    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    sudo eopkg install neovim
+    curl -fLo $HOME/dotfiles/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   else
     echo_item "Skipping Neovim install" red
   fi
@@ -91,15 +75,67 @@ echo ""
 
 
 
-# -- Plank --------------------------------------------------------------------
+# -- Ctags --------------------------------------------------------------------
 
-if exists "plank"; then
-  echo_item "Plank already installed" green
+if exists "ctags"; then
+  echo_item "Ctags is already installed" "green"
 else
-  sudo add-apt-repository ppa:ricotz/docky
-  sudo apt-get update
-  sudo apt-get install plank --yes --force-yes
+  sudo eopkg install ctags
 fi
+
+echo ""
+
+
+# -- Ripgrep --------------------------------------------------------------------
+
+if exists "ripgrep"; then
+  echo_item "Ripgrep is already installed" "green"
+else
+  sudo eopkg install ripgrep
+fi
+
+echo ""
+
+
+
+# -- jq --------------------------------------------------------------------
+
+if exists "jq"; then
+  echo_item "jq is already installed" "green"
+else
+  sudo eopkg install jq
+fi
+
+echo ""
+
+# -- Tmux --------------------------------------------------------------------
+
+if exists "tmux"; then
+  echo_item "Tmux is already installed" "green"
+else
+  sudo eopkg install tmux
+fi
+
+echo ""
+
+
+
+# -- Nordic GTK --------------------------------------------------------------------
+
+sudo rm -rf /usr/share/themes/Nordic
+sudo git clone https://github.com/EliverLara/Nordic.git /usr/share/themes/Nordic
+gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+
+echo ""
+
+
+
+# -- Zafiro Icons --------------------------------------------------------------------
+
+sudo rm -rf /usr/share/icons/Zafiro
+sudo git clone https://github.com/zayronxio/Zafiro-icons.git /usr/share/icons/Zafiro
+gsettings set org.gnome.desktop.interface icon-theme 'Zafiro'
 
 echo ""
 
@@ -107,10 +143,87 @@ echo ""
 
 # -- Plank --------------------------------------------------------------------
 
-if exists "flat-remix"; then
-  echo_item "Flat remix icons already installed"
+if exists "plank"; then
+  echo_item "Plank is already installed" "green"
 else
-  sudo add-apt-repository ppa:daniruiz/flat-remix
-  sudo apt-get update
-  sudo apt-get install flat-remix
+  sudo eopkg install plank
 fi
+
+
+
+# -- Tilix --------------------------------------------------------------------
+
+if exists "tilix"; then
+  echo_item "Tilix is already installed" "green"
+else
+  sudo eopkg install tilix
+  git clone --depth=1 https://github.com/arcticicestudio/nord-tilix.git ./tmp/nord
+  sudo mv ./tmp/nord/src/json/nord.json /usr/share/tilix/schemes/nord.json
+  rm -rf ./tmp
+fi
+
+echo ""
+
+
+
+# -- Slack --------------------------------------------------------------------
+
+if exists "slack"; then
+  echo_item "Slack is already installed" "green"
+else
+  sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/im/slack-desktop/pspec.xml
+  sudo eopkg it slack-desktop*.eopkg;sudo rm slack-desktop*.eopkg
+  # Todo use this: https://raw.githubusercontent.com/arcticicestudio/nord-tilix/develop/src/json/nord.json
+fi
+
+echo ""
+
+
+# -- Google Chrome --------------------------------------------------------------------
+
+if exists "google-chrome-stable"; then
+  echo_item "Google chrome is already installed" "green"
+else
+  sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
+sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
+fi
+
+echo ""
+
+
+
+# -- Docker --------------------------------------------------------------------
+
+if exists "docker"; then
+  echo_item "Docker is already installed" "green"
+else
+  sudo eopkg install docker
+  sudo usermod -aG docker $USER
+fi
+
+echo ""
+
+
+
+# -- Docker Compose --------------------------------------------------------------------
+
+if exists "docker-compose"; then
+  echo_item "Docker-compose is already installed" "green"
+else
+  sudo eopkg install docker-compose
+  sudo usermod -aG docker-compose $USER
+fi
+
+echo ""
+
+
+
+# -- Make --------------------------------------------------------------------
+
+if exists "make"; then
+  echo_item "Make is already installed" "green"
+else
+  sudo eopkg install make
+fi
+
+echo ""
