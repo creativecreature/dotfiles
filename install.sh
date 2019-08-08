81791a931ee3c31338c3b486392badc13f4653d2
Victor Conner <charlesvictorconner@gmail.com>
2019-08-08 14:17:24 +0200

    Slightly changed formatting

--- install.sh
+++ install.sh
@@ -2,13 +2,11 @@
 # Install .dotfiles
 
 # -- Import from other scripts -------------------------------------------------
-
 source 'scripts/helpers.sh'
 
 
 
 # -- OSX- or Linux-Specific Setup ----------------------------------------------
-
 if system_is_OSX; then
   source 'osx/osx.sh'
 elif system_is_linux; then
@@ -19,7 +17,6 @@ fi
 
 
 # -- GIT -----------------------------------------------------------------------
-
 ln -sf $HOME/dotfiles/git/gitignore_global $HOME/.gitignore_global
 echo_item "Linked global .gitignore" "green"
 ln -sf $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
@@ -29,7 +26,6 @@ echo ""
 
 
 # -- ZSH Setup -----------------------------------------------------------------
-
 if exists "zsh"; then
   if get_boolean_response "Do you want to install ZSH configuration files?"; then
 
@@ -62,7 +58,6 @@ echo ""
 
 
 # -- NVIM -----------------------------------------------------------------------
-
 rm -rf $HOME/.config/nvim
 ln -sfn $HOME/dotfiles/nvim $HOME/.config/nvim
 echo_item "Linked nvim configuration" "green"
@@ -72,7 +67,6 @@ echo ""
 
 
 # -- TMUX -----------------------------------------------------------------------
-
 ln -sf $HOME/dotfiles/tmux/conf $HOME/.tmux.conf
 echo_item "Linked tmux configuration" "green"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -85,7 +79,6 @@ echo ""
 
 
 # -- ESLINT -----------------------------------------------------------------------
-
 ln -sf $HOME/dotfiles/eslint/eslintrc $HOME/.eslintrc
 echo_item "Linked global eslint config" "green"
 
@@ -94,7 +87,6 @@ echo ""
 
 
 # -- RIPGREP -----------------------------------------------------------------------
-
 ln -sf $HOME/dotfiles/ripgrep/ignore $HOME/.ignore
 echo_item "Linked ripgrep ignore file" "green"
 
