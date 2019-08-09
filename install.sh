a19c1b5ee79e5f8bc5d6df4df473dc258b6281cc
creativecreature <hello@creativecreature.com>
2019-08-09 18:12:10 +0200

    refactored zsh setup into its own bash script

--- install.sh
+++ install.sh
@@ -52,40 +52,13 @@ source 'git/git.sh'
 
 
 
-# -- GIT -----------------------------------------------------------------------
+# -- SSH -----------------------------------------------------------------------
 source 'ssh/ssh.sh'
 
 
 
 # -- ZSH Setup -----------------------------------------------------------------
-if exists "zsh"; then
-  if get_boolean_response "Do you want to install ZSH configuration files?"; then
-
-    # -- ZSHRC
-    ln -sf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
-    echo_item "Linked zshrc" "green"
-
-    # -- OH MY ZSH
-    if [ -d $HOME/.oh-my-zsh/ ]; then
-      echo_item "Oh my ZSH is already installed" "green"
-    else
-      if exists "curl"; then
-        curl -L http://install.ohmyz.sh | sh
-      elif exists "wget"; then
-        wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-      else
-        echo_item "You need either curl or wget installed to download Oh My ZSH"
-      fi
-    fi
-
-  else
-    echo_item "Ignoring ZSH configuration" "red"
-  fi
-else
-  echo_item "ZSH is not installed"
-fi
-
-echo ""
+source 'zsh/zsh.sh'
 
 
 
