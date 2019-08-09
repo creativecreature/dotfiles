562def4d1c49beecbc97b3e486be91e7a9e14771
Victor Conner <charlesvictorconner@gmail.com>
2019-08-09 12:13:59 +0200

    Directory structure and ssh setup

--- install.sh
+++ install.sh
@@ -6,6 +6,37 @@ source 'scripts/helpers.sh'
 
 
 
+# -- Setup directory structure -------------------------------------------------
+echo_header "Setting up source code directory structure"
+CODE_DIR=$HOME/code
+VIAPLAY_DIR=$HOME/code/viaplay
+CONNER_DIR=$HOME/code/conner
+
+if [[ -d "$CODE_DIR" ]]; then
+  echo_item "Code dir already exists" "green"
+else
+  echo_item "Creating code dir" "green"
+  mkdir $HOME/code
+fi
+
+if [[ -d "$VIAPLAY_DIR" ]]; then
+  echo_item "Viaplay dir already exists" "green"
+else
+  echo_item "Creating viaplay dir" "green"
+  mkdir $HOME/viaplay
+fi
+
+if [[ -d "$CONNER_DIR" ]]; then
+  echo_item "Conner dir already exists" "green"
+else
+  echo_item "Creating conner dir" "green"
+  mkdir $HOME/conner
+fi
+
+echo ""
+
+
+
 # -- OSX- or Linux-Specific Setup ----------------------------------------------
 if system_is_OSX; then
   source 'osx/osx.sh'
