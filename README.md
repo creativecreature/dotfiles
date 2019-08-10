820d6e010ace5dc532cf0d8ccc78253c6b847bd9
Victor Conner <charlesvictorconner@gmail.com>
2019-08-10 12:10:10 +0200

    Minor refactor

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotfiles
+
+I use this to prevent configuration drifts between different machines I work on.
+
+To use it simply clone this repository to your home directory and run `./install.sh`.
--- nvim/nvim.sh
+++ nvim/nvim.sh
@@ -8,13 +8,13 @@ echo_item "Symlinking nvim configuration" "green"
 rm -rf $HOME/.config/nvim
 ln -sfn $HOME/dotfiles/nvim $HOME/.config/nvim
 
-echo_item "Installing NVIM plugins"
+echo_item "Installing NVIM plugins" "green"
 nvim "+PlugInstall" +qall
 
-echo_item "Updating NVIM plugins"
+echo_item "Updating NVIM plugins" "green"
 nvim "+PlugUpdate" +qall
 
-echo_item "Installing coc sources"
+echo_item "Installing coc sources" "green"
 nvim "+CocInstall coc-tsserver coc-eslint coc-json coc-prettier coc-css coc-emoji" +qall
 
 echo ""
