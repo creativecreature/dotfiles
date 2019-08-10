d9af1add18345619d0c13e9d1f671f4c7447e62c
creativecreature <hello@creativecreature.com>
2019-08-10 11:38:19 +0200

    NVIM installation script

--- nvim/nvim.sh
+++ nvim/nvim.sh
@@ -3,8 +3,18 @@
 source 'scripts/helpers.sh'
 
 echo_header "NVIM Configuration"
-echo_item "Linked nvim configuration" "green"
+
+echo_item "Symlinking nvim configuration" "green"
 rm -rf $HOME/.config/nvim
 ln -sfn $HOME/dotfiles/nvim $HOME/.config/nvim
+
+echo_item "Installing NVIM plugins"
+nvim "+PlugInstall" +qall
+
+echo_item "Updating NVIM plugins"
+nvim "+PlugUpdate" +qall
+
+echo_item "Installing coc sources"
+nvim "+CocInstall coc-tsserver coc-eslint coc-json coc-prettier coc-css coc-emoji" +qall
 
 echo ""
--- terminal/terminal.sh
+++ terminal/terminal.sh
@@ -3,7 +3,12 @@
 source './scripts/helpers.sh'
 
 echo_header "Terminal configuration"
-echo_item "Symlinking GTK styles for the terminal"
+
+echo_item "Symlinking GTK styles for the terminal" "green"
 ln -sfn $HOME/dotfiles/terminal/gtk.css $HOME/.config/gtk-3.0/gtk.css
+
+echo_item "Setting up terminal colors" "green"
+infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > $TERM.ti
+tic $TERM.ti
 
 echo ""
