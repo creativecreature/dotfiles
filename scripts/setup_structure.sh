eeaf710b599cef2905c3614d10765e328f6eaa9d
creativecreature <hello@creativecreature.com>
2019-08-14 07:24:54 +0200

    Tmux for ubuntu

--- scripts/setup_structure.sh
+++ scripts/setup_structure.sh
@@ -3,6 +3,7 @@
 CODE_DIR=$HOME/code
 VIAPLAY_DIR=$HOME/code/viaplay
 CONNER_DIR=$HOME/code/conner
+BIN_DIR=$HOME/bin
 
 echo_header "Setting up source code directory structure"
 
@@ -17,14 +18,20 @@ if [[ -d "$VIAPLAY_DIR" ]]; then
   echo_item "Viaplay dir already exists" "green"
 else
   echo_item "Creating viaplay dir" "green"
-  mkdir $HOME/viaplay
+  mkdir $HOME/code/viaplay
 fi
 
 if [[ -d "$CONNER_DIR" ]]; then
   echo_item "Conner dir already exists" "green"
 else
   echo_item "Creating conner dir" "green"
-  mkdir $HOME/conner
+  mkdir $HOME/code/conner
 fi
 
+if [[ -d "$BIN_DIR" ]]; then
+  echo_item "Bin dir already exists" "green"
+else
+  echo_item "Creating bin dir" "green"
+  mkdir $HOME/bin
+fi
 echo ""
