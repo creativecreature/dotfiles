d1ffa20edb59a2f7684bff25044d3600bb34cf9e
Victor Conner <charlesvictorconner@gmail.com>
2019-08-16 08:29:15 +0200

    Updated ubuntu script and added dock setup

---
+++ dock/dock.sh
@@ -0,0 +1,8 @@
+# MOVE TO BOTTOM AND DONT BE MAX WIDTH
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
+# AUTO HIDE DOCK
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
+
