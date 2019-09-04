65fed9acf89b2635f016c2bbf943280463b9d1e1
Victor Conner <charlesvictorconner@gmail.com>
2019-09-04 10:54:04 +0200

    eslint installation

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -48,7 +48,7 @@ else
     NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     nvm install 12.2.0
-    npm install -g eslint eslint-plugin-react babel-eslint yarn neovim
+    npm install -g eslinteslint@5.16 eslint-config-prettier@6.0.0 eslint-config-standard@12.0.0 eslint-plugin-import@2.18.0 eslint-plugin-jest@22.7.2 eslint-plugin-node@9.10 eslint-plugin-promise@4.2.1 eslint-plugin-standard@4.0.0 eslint-plugin-react babel-eslint yarn neovim
   else
     echo_item "Skipping Node.js tools install" red
   fi
