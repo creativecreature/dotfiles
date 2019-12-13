16929a712266e0cbb1e5139984b67800c25234db
creativecreature <hello@creativecreature.com>
2019-12-13 10:11:43 +0100

    Temporairly remove sudos for running headless in docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,4 +23,6 @@ cd $HOME/code/conner
 git clone git@github.com:CharlesConner/dotfiles.git
 ln -s $HOME/code/conner/dotfiles $HOME/dotfiles
 cd dotfiles
+# Temporairly test this for running headless
+find . -type f -print0 | xargs -0 sed -i 's/sudo //g'
 ./install.sh
