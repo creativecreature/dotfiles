38b1a7100f1bb80e5a486096d401643942bb8a07
Victor Conner <charlesvictorconner@gmail.com>
2019-12-13 11:46:32 +0100

    Update copy command

--- headless.sh
+++ headless.sh
@@ -6,4 +6,4 @@ export HEADLESS=true
 source 'scripts/setup_structure.sh'
 
 find . -type f -print0 | xargs -0 sed -i 's/sudo //g'
-cp -r $pwd /home/$USER_NAME/dotfiles
+cp -r . /home/$USER_NAME/dotfiles
