74a963bf218562a52aa74a4a295ef76864331651
Victor Conner <charlesvictorconner@gmail.com>
2019-12-13 09:52:56 +0100

    Remove optional node installment

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -42,16 +42,12 @@ echo ""
 if exists "node"; then
   echo_item "Node tools are already installed" green
 else
-  if get_boolean_response "Do you want to install Node.js tools?"; then
-    mkdir ~/.nvm
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-    NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    nvm install 12.2.0
-    npm install -g eslint@5.16 eslint-config-prettier@6.0.0 eslint-config-standard@12.0.0 eslint-plugin-import@2.18.0 eslint-plugin-jest@22.7.2 eslint-plugin-node@9 eslint-plugin-promise@4.2.1 eslint-plugin-standard@4.0.0 eslint-plugin-react babel-eslint yarn neovim
-  else
-    echo_item "Skipping Node.js tools install" red
-  fi
+  mkdir ~/.nvm
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+  NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  nvm install 12.2.0
+  npm install -g eslint@5.16 eslint-config-prettier@6.0.0 eslint-config-standard@12.0.0 eslint-plugin-import@2.18.0 eslint-plugin-jest@22.7.2 eslint-plugin-node@9 eslint-plugin-promise@4.2.1 eslint-plugin-standard@4.0.0 eslint-plugin-react babel-eslint yarn neovim
 fi
 
 echo ""
@@ -374,10 +370,10 @@ else
   echo_item "Installing Insomnia" "green"
   echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
     | sudo tee -a /etc/apt/sources.list.d/insomnia.list
-  wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
-    | sudo apt-key add -
-  sudo apt-get update
-  sudo apt-get install insomnia
+      wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+        | sudo apt-key add -
+              sudo apt-get update
+              sudo apt-get install insomnia
 fi
 
 
