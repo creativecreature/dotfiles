7d3dd9b3fa58f16373b8a5db39defaee34965eda
creativecreature <hello@creativecreature.com>
2019-12-13 11:26:36 +0100

    Remove green from setup dirs

--- scripts/setup_structure.sh
+++ scripts/setup_structure.sh
@@ -10,37 +10,37 @@ WALLPAPER_DIR=/home/$USER_NAME/wallpaper
 echo "Setting up source code directory structure"
 
 if [[ -d "$CODE_DIR" ]]; then
-  echo "Code dir already exists" "green"
+  echo "Code dir already exists"
 else
-  echo "Creating code dir" "green"
+  echo "Creating code dir"
   mkdir /home/$USER_NAME/code
 fi
 
 if [[ -d "$VIAPLAY_DIR" ]]; then
-  echo "Viaplay dir already exists" "green"
+  echo "Viaplay dir already exists"
 else
-  echo "Creating viaplay dir" "green"
+  echo "Creating viaplay dir"
   mkdir /home/$USER_NAME/code/viaplay
 fi
 
 if [[ -d "$USER_DIR" ]]; then
-  echo "Username dir already exists" "green"
+  echo "Username dir already exists"
 else
-  echo "Creating username code dir" "green"
+  echo "Creating username code dir"
   mkdir /home/$USER_NAME/code/$USER_NAME
 fi
 
 if [[ -d "$BIN_DIR" ]]; then
-  echo "Bin dir already exists" "green"
+  echo "Bin dir already exists"
 else
-  echo "Creating bin dir" "green"
+  echo "Creating bin dir"
   mkdir /home/$USER_NAME/bin
 fi
 
 if [[ -d "$INSTALL_DIR" ]]; then
-  echo "Dotfile-installs directory already exists" "green"
+  echo "Dotfile-installs directory already exists"
 else
-  echo "Creating dotfile-installs directory" "green"
+  echo "Creating dotfile-installs directory"
   mkdir "$INSTALL_DIR"
 fi
 
