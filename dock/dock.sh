503b0a842d153c09fc90eb7f27f9dd46a02b4233
Victor Conner <charlesvictorconner@gmail.com>
2019-12-14 19:56:58 +0100

    Add shebang

--- dock/dock.sh
+++ dock/dock.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # MOVE TO BOTTOM AND DONT BE MAX WIDTH
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
--- zsh/utilities.sh
+++ zsh/utilities.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Swedish for find
 hitta() {
