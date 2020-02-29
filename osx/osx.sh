e0cc7657c8fae2a2bcc377cd9f54efcae63ebc11
creativecreature <hello@creativecreature.com>
2020-02-29 13:18:31 +0100

    Add bat theme

--- osx/osx.sh
+++ osx/osx.sh
@@ -251,6 +251,9 @@ if exists "bat"; then
 else
   echo_item "Installing Bat" "green"
   brew install bat
+  mkdir -p "$(bat --config-dir)/themes"
+  ln -sf ~/dotfiles/bat/palenight.tmTheme "$(bat --config-dir)/themes/palenight.tmTheme"
+  bat cache --build
 fi
 
 echo ""
