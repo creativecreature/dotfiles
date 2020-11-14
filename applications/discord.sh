f7b702051a5fb94b10f8cd9127fed3672189a4b9
creativecreature <hello@creativecreature.com>
2020-11-14 10:59:12 +0100

    Discord installation

---
+++ applications/discord.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+install_discord() {
+  if exists "discord"; then
+    echo_item "discord is already installed" "green"
+  else
+    echo_item "installing discord" "green"
+    sudo snap install discord > /dev/null
+  fi
+}
