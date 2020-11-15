075e3b3aa989cece6b5899ddb1d2ceb7ed7d9043
creativecreature <hello@creativecreature.com>
2020-11-15 17:41:50 +0100

    Add keyboard shortcut to open enpass

--- applications/enpass.sh
+++ applications/enpass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 install_enpass() {
-  if [[ $(dpkg -l | grep 'enpass' >/dev/null) ]]; then
+  if [[ $(dpkg -l | grep 'enpass') ]]; then
     echo_item "enpass is already installed" "green"
   else
     echo_item "installing Enpass" "green"
@@ -10,4 +10,11 @@ install_enpass() {
     sudo apt-get -qq --yes update
     sudo apt-get -qq --yes install enpass
   fi
+}
+
+configure_enpass() {
+  echo_item "setting up enpass shortcut" "green"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Enpass'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command '/opt/enpass/Enpass showassistant'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Alt><Super>slash'
 }
