097806b66479b6fb3c2853f6d42cfb72f1f8264d
Victor Conner <charlesvictorconner@gmail.com>
2020-12-17 17:46:10 +0100

    Added tje iser to the keyboard input group

--- hardware/keyboard.sh
+++ hardware/keyboard.sh
@@ -3,4 +3,6 @@
 configure_keyboard() {
   echo_item "adding US and SE keyboard input sources" "green"
   gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'se')]"
+  echo_item "adding the user to the dialout group" "green"
+  sudo usermod -a -G dialout $USER # Required for Bazecor
 }
