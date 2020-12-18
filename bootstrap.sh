845490424db8661d35b77728cae31996dd0213f4
Victor Conner <charlesvictorconner@gmail.com>
2020-12-18 16:31:41 +0100

    Remove USER directory and extra ssh key

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,12 +11,12 @@ sudo apt-get -qq --yes update
 install_git
 
 # Clone dotfiles repo and symlink it to the users home directory
-git clone https://github.com/CharlesConner/dotfiles.git ~/code/$USER/dotfiles
-ln -s ~/code/$USER/dotfiles ~/dotfiles
+git clone https://github.com/creativecreature/dotfiles.git ~/code/creativecreature/dotfiles
+ln -s ~/code/creativecreature/dotfiles ~/dotfiles
 cd ~/dotfiles && ./install.sh
 
 # Post installation I want to update the remote to be ssh instead of https
-cd ~/dotfiles && git remote set-url origin git@github.com:charlesconner/dotfiles.git
+cd ~/dotfiles && git remote set-url origin git@github.com:creativecreature/dotfiles.git
 
 # Installation complete, ask for reboot
 echo "Installation completed successfully."
--- setup_structure.sh
+++ setup_structure.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 CODE_DIR=~/code
-USER_DIR=~/code/$USER
 CREATIVE_CREATURE_DIR=~/code/creativecreature
 BIN_DIR=~/bin
 CONFIG_DIR=~/.config
 TMP_DIR=~/tmp
 
 mkdir -p $CODE_DIR
-mkdir -p $USER_DIR
 mkdir -p $CREATIVE_CREATURE_DIR
 mkdir -p $BIN_DIR
 mkdir -p $CONFIG_DIR
