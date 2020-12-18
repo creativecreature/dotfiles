66ba9b6076cd77d79a9d527ddf2f83f0002717e2
creativecreature <hello@creativecreature.com>
2020-12-18 16:31:41 +0100

    Remove USER directory and extra ssh key

--- utilities/ssh/ssh.sh
+++ utilities/ssh/ssh.sh
@@ -4,7 +4,6 @@ configure_ssh() {
   SSH_DIR=~/.ssh
   SSH_CONFIG=~/.ssh/config
   CREATIVE_CREATURE_KEY_PUB=~/.ssh/creativecreature.pub
-  PERSONAL_KEY_PUB=~/.ssh/private_rsa.pub
 
   mkdir -p $SSH_DIR
 
@@ -13,13 +12,6 @@ configure_ssh() {
   else
     echo_item "generating a ssh key for creativecreature" "green"
     ssh-keygen -t rsa -b 4096 -f ~/.ssh/creativecreature -C "hello@creativecreature.com"
-  fi
-
-  if [[ -f "$PERSONAL_KEY_PUB" || -L "$PERSONAL_KEY_PUB" ]]; then
-    echo_item "personal ssh key already exists" "green"
-  else
-    echo_item "generating personal ssh key" "green"
-    ssh-keygen -t rsa -b 4096 -f ~/.ssh/private_rsa -C "charlesvictorconner@gmail.com"
   fi
 
   echo_item "symlinking ssh config to ~/.ssh/config" "green"
