571d862a20a891c3b657c40a0dda7ed6811744e5
creativecreature <hello@creativecreature.com>
2021-07-14 17:03:09 +0200

    Install additional packages to be able to compile Alacritty

--- terminal/alacritty/alacritty.sh
+++ terminal/alacritty/alacritty.sh
@@ -9,7 +9,7 @@ install_alacritty() {
     cd ~/alacritty
     rustup override set stable
     rustup update stable
-    sudo apt-get -qq --yes install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+    sudo apt-get -qq --yes install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
     cargo build --release
     sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
     sudo cp target/release/alacritty /usr/local/bin
