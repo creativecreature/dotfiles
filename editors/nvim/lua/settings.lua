77eced341db3f58432319f2b85c1f9db76e74e37
creativecreature <hello@creativecreature.com>
2021-08-27 19:52:25 +0200

    Made a few minor improvements to the lua config

--- editors/nvim/lua/settings.lua
+++ editors/nvim/lua/settings.lua
@@ -2,28 +2,27 @@ local cmd = vim.cmd
 local opt = vim.opt
 
 opt.shell='zsh' -- Set zsh as the prompt for vim
-opt.history=10000
--- opt.noshowmode=true -- Hide the mode in the cmd window
-vim.opt.listchars = {eol = '↲', tab = '▸ ', trail = '·'}
+opt.history=10000 -- Increase the history
+opt.swapfile=false -- Dont use swap files
 opt.hidden=true
-cmd [[
-  set noshowmode
-  set noswapfile
-]]
-opt.numberwidth=5
 opt.cmdheight=2 -- Give more space for displaying messages.
 opt.inccommand='nosplit' -- Show % substitution changes without having to hit enter
-opt.number=true
-opt.relativenumber=true
+opt.showmode=false -- Dont show the mode in the command window
+vim.opt.listchars = {eol = '↲', tab = '▸ ', trail = '·'}
+
+-- Line numbers
+opt.number=true -- Show the current line as the real line number
+opt.relativenumber=true -- Show other lines relative to the current line
 
 -- Indentation
-opt.shiftwidth=2 -- number of spaces to use for autoindenting
-opt.tabstop=2 -- number of space characters to insert when tab is pressed
+opt.shiftwidth=2 -- Number of spaces to use for autoindenting
+opt.tabstop=2 -- Number of space characters to insert when tab is pressed
 opt.softtabstop=2 -- <Tab> and <BS> inserts and deletes right amount of spaces
-opt.expandtab=true -- insert space characters whenever tab key is pressed
+opt.expandtab=true -- Insert space characters whenever tab key is pressed
 
 -- Better copy pasting
 opt.clipboard='unnamed,unnamedplus'
+
 -- Searching
 opt.ignorecase=true
 opt.smartcase=true
