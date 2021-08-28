dd91cdcf3fc29f2cf87624c68d4adec59f60c383
creativecreature <hello@creativecreature.com>
2021-08-28 12:39:40 +0200

    Sort buffers on last used

--- editors/nvim/lua/plugins/telescope.lua
+++ editors/nvim/lua/plugins/telescope.lua
@@ -6,5 +6,21 @@ local actions = require('telescope.actions')require('telescope').setup{
         ["<esc>"] = actions.close,
       },
     },
+  },
+  pickers = {
+    -- Your special builtin config goes in here
+    buffers = {
+      sort_lastused = true,
+      mappings = {
+        i = {
+          ["<c-d>"] = require("telescope.actions").delete_buffer,
+          -- Right hand side can also be the name of the action as a string
+          ["<c-d>"] = "delete_buffer",
+        },
+        n = {
+          ["<c-d>"] = require("telescope.actions").delete_buffer,
+        }
+      }
+    },
   }
 }
