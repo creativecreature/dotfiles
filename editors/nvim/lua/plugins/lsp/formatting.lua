7173b1c6f1f8250d27b2efe2f985b1087ef94efe
creativecreature <hello@creativecreature.com>
2021-11-16 10:19:55 +0100

    Fixes to run on OSX

---
+++ editors/nvim/lua/plugins/lsp/formatting.lua
@@ -0,0 +1,49 @@
+local util = require("util")
+
+local M = {}
+
+-- vim.lsp.handlers["textDocument/hover"] = function(_, method, result)
+--   print(vim.inspect(result))
+-- end
+
+M.autoformat = false
+
+function M.toggle()
+  M.autoformat = not M.autoformat
+  if M.autoformat then
+    util.info("enabled format on save", "Formatting")
+  else
+    util.warn("disabled format on save", "Formatting")
+  end
+end
+
+function M.format()
+  if M.autoformat then
+    vim.lsp.buf.formatting_sync()
+  end
+end
+
+function M.setup(client, buf)
+  local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+  local nls = require("plugins.lsp.null-ls")
+
+  local enable = false
+  if nls.has_formatter(ft) then
+    enable = client.name == "null-ls"
+  else
+    enable = not (client.name == "null-ls")
+  end
+
+  client.resolved_capabilities.document_formatting = enable
+  -- format on save
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd([[
+      augroup LspFormat
+        autocmd! * <buffer>
+        autocmd BufWritePre <buffer> lua require("plugins.lsp.formatting").format()
+      augroup END
+    ]])
+  end
+end
+
+return M
---
+++ editors/nvim/lua/plugins/lsp/null-ls.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+function M.setup(options)
+  local nls = require("null-ls")
+  nls.config({
+    debounce = 150,
+    save_after_format = false,
+    sources = {
+      nls.builtins.formatting.prettierd,
+      nls.builtins.formatting.stylua,
+      nls.builtins.formatting.fish_indent,
+      nls.builtins.formatting.fixjson.with({ filetypes = { "jsonc" } }),
+      -- nls.builtins.formatting.eslint_d,
+      nls.builtins.diagnostics.shellcheck,
+      nls.builtins.diagnostics.markdownlint,
+      nls.builtins.diagnostics.selene,
+      nls.builtins.code_actions.gitsigns,
+    },
+  })
+  require("lspconfig")["null-ls"].setup(options)
+end
+
+function M.has_formatter(ft)
+  local config = require("null-ls.config").get()
+  local formatters = config._generators["NULL_LS_FORMATTING"]
+  for _, f in ipairs(formatters) do
+    if vim.tbl_contains(f.filetypes, ft) then
+      return true
+    end
+  end
+end
+
+return M
