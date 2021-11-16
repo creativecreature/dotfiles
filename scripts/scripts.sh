742d55d3b3c6fccfc2bac5b5aef6ba32a00e199a
creativecreature <hello@creativecreature.com>
2021-11-16 10:16:09 +0100

    Updated the scripts

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 install_scripts() {
-  echo_item "Installing scripts" "green"
   for filepath in ~/dotfiles/scripts/*; do
     [[ -e "$filepath" ]] && ! [[ "$filepath" =~ scripts\.sh ]] || continue
     filename=${filepath##*/}
