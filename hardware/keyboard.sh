28d65cd1ede8967cddabe3bd143a84a3722765e1
Victor Conner <hello@creativecreature.com>
2021-11-22 16:36:29 +0100

    Fix key repeat on mac

--- hardware/keyboard.sh
+++ hardware/keyboard.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 configure_keyboard() {
-  echo_item "adding US and SE keyboard input sources" "green"
-  gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'se')]"
-  echo_item "adding the user to the dialout group" "green"
-  sudo usermod -a -G dialout $USER # Required for Bazecor
+	# Set faster key repeat see https://mac-os-key-repeat.vercel.app/
+	defaults write -g InitialKeyRepeat -int 4
+	defaults write -g KeyRepeat -int 1
 }
