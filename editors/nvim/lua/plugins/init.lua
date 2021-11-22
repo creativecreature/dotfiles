3b00172a9b09bfd728c12e5a234504a8b4aed55b
Victor Conner <hello@creativecreature.com>
2021-11-22 14:54:24 +0100

    VIM test typescript pattern

--- editors/nvim/lua/plugins/init.lua
+++ editors/nvim/lua/plugins/init.lua
@@ -94,5 +94,6 @@ end)
 
 vim.cmd [[
   let g:test#javascript#runner = 'jest'
+  let g:test#javascript#jest#file_pattern = '\v(__tests__/.*|(spec|test))\.(js|jsx|coffee|ts|tsx)$'
   let test#strategy = "vimux"
 ]]
