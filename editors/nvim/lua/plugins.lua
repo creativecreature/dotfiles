89ee5ec286a8b18b5f1a9c5447a50d74521726d3
Victor Conner <hello@creativecreature.com>
2021-12-23 15:48:39 +0100

    Refactored the code for packer

--- editors/nvim/lua/plugins.lua
+++ editors/nvim/lua/plugins.lua
@@ -1,14 +1,35 @@
 local fn = vim.fn
 
--- Setup packer
-local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  PACKER_BOOTSTRAP = fn.system {
+    "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
 end
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	print "Failed to require packer"
+	return
+end
+
+-- Have packer use a popup window
+packer.init {display = {open_fn = function() return require("packer.util").float {border = "rounded"} end}}
+
 -- Install plugins
-require('packer').startup(function(use)
+return packer.startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- UI
@@ -89,10 +110,12 @@ require('packer').startup(function(use)
   -- Wakatime
   use 'wakatime/vim-wakatime'
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then require("packer").sync() end
 end)
-
-vim.cmd [[
-  let g:test#javascript#runner = 'jest'
-  let g:test#javascript#jest#file_pattern = '\v(__tests__/.*|(spec|test))\.(js|jsx|coffee|ts|tsx)$'
-  let test#strategy = "vimux"
-]]
+-- vim.cmd [[
+--   let g:test#javascript#runner = 'jest'
+--   let g:test#javascript#jest#file_pattern = '\v(__tests__/.*|(spec|test))\.(js|jsx|coffee|ts|tsx)$'
+--   let test#strategy = "vimux"
+-- ]]
