38191b6f9f2379e6ee88c6c5eb22c87a44fc7506
Victor Conner <hello@creativecreature.com>
2022-04-11 10:01:16 +0200

    Fix a small issue with cmp completions

--- editors/nvim/lua/plugins/cmp.lua
+++ editors/nvim/lua/plugins/cmp.lua
@@ -9,7 +9,7 @@ cmp.setup({
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({select = true}),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'}),
     ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})
   },
