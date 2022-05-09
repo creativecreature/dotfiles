84e5a7d8044c7ff021159d4ae31efcfe493cf6ff
Victor Conner <hello@creativecreature.com>
2022-05-09 08:18:30 +0200

    Formatting

--- editors/nvim/lua/plugins.lua
+++ editors/nvim/lua/plugins.lua
@@ -45,9 +45,8 @@ return packer.startup(function(use)
   })
   use({"edluffy/specs.nvim", after = "neoscroll.nvim", config = function() require("plugins.specs") end})
 
-  -- Syntax
-	use "folke/lua-dev.nvim"
-	use "williamboman/nvim-lsp-installer"
+  use "folke/lua-dev.nvim"
+  use "williamboman/nvim-lsp-installer"
   use {'neovim/nvim-lspconfig', opt = true, event = "BufReadPre", config = function() require('plugins.lspconfig') end}
   use {
     'nvim-treesitter/nvim-treesitter',
@@ -63,8 +62,9 @@ return packer.startup(function(use)
     "hrsh7th/nvim-cmp",
     config = function() require("plugins.cmp") end,
     requires = {
-      "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-vsnip", "hrsh7th/vim-vsnip",
-      "hrsh7th/cmp-emoji", {"windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup({ map_cr = false }) end}
+      "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip", "hrsh7th/cmp-emoji",
+      {"windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup({map_cr = false}) end}
     }
   })
   use 'tpope/vim-repeat'
