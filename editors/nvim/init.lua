4dc31a9fe692d39d6b637d6cbfae6f8d05f3a6bc
Victor Conner <hello@creativecreature.com>
2022-05-27 07:47:27 +0200

    Updated the checkhealth providers

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -1,8 +1,6 @@
 local cmd = vim.cmd
 cmd [[
-	let g:loaded_python_provider = 0
-	let g:python3_host_prog = '/usr/bin/python3'
-	let g:node_host_prog = '/Users/conner/.volta/bin/node'
+	let g:loaded_ruby_provider = 0
 ]]
 require('plugins')
 require('settings')
