f2414c93dddadfebb5bea5d988e31191491ba366
Victor Conner <hello@creativecreature.com>
2022-06-20 15:48:43 +0200

    Add keybinding for scroll docs

--- editors/nvim/lua/plugins/cmp.lua
+++ editors/nvim/lua/plugins/cmp.lua
@@ -7,14 +7,21 @@ cmp.setup({
 	snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
 	preselect = cmp.PreselectMode.None,
 	mapping = {
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }),
 		['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
 		['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
 	},
-	sources = { { name = "nvim_lsp" }, { name = "nvim_lua" }, { name = 'luasnip' }, { name = "buffer" }, { name = "path" }, { name = 'emoji' } },
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = 'luasnip' },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = 'emoji' }
+	},
 	window = {
 		documentation = { border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" } },
 	},
