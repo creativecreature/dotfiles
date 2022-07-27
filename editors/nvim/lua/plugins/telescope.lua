11ae1e11f19e08c35ac902d032de4306d27e094c
Victor Conner <hello@creativecreature.com>
2022-07-27 11:16:42 +0200

    Update the look and feel for telescope

--- editors/nvim/lua/plugins/telescope.lua
+++ editors/nvim/lua/plugins/telescope.lua
@@ -5,18 +5,27 @@ telescope.load_extension('fzf')
 telescope.load_extension("live_grep_args")
 
 telescope.setup {
-  defaults = {
-    mappings = {
-      i = {["<esc>"] = actions.close} -- Close on escape
-    }
-  },
-  pickers = {
-    buffers = {
-      sort_lastused = true,
-      mappings = {
-        i = {["<c-d>"] = require("telescope.actions").delete_buffer},
-        n = {["<c-d>"] = require("telescope.actions").delete_buffer}
-      }
-    }
-  }
+	defaults = {
+		previewer = false,
+		layout_strategy = "bottom_pane", -- Puts the window at the bottom
+		layout_config = {
+			prompt_position = "bottom", -- Puts the cursor at the bottom of the window
+			height = 0.3
+		},
+		mappings = {
+			i = { ["<esc>"] = actions.close } -- Close on escape
+		}
+	},
+	pickers = {
+		find_files = {
+			previewer = false,
+		},
+		buffers = {
+			sort_lastused = true,
+			mappings = {
+				i = { ["<c-d>"] = require("telescope.actions").delete_buffer },
+				n = { ["<c-d>"] = require("telescope.actions").delete_buffer }
+			}
+		}
+	}
 }
