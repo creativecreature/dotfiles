98ca29e0bdb1c8c8d482ffd5d49f251d95bfb1b7
Victor Conner <hello@creativecreature.com>
2023-04-04 10:02:32 +0200

    Switch to use null-ls

--- editors/nvim/lua/plugins/lspconfig.lua
+++ editors/nvim/lua/plugins/lspconfig.lua
@@ -1,102 +1,138 @@
 require("neodev").setup({})
-local lspconfig = require('lspconfig')
-local util = require('lspconfig/util')
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
 
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. See the documentation for more information.
 local servers = {
-  tsserver = {},
-  gopls = {},
-  bashls = {},
-  dockerls = {},
-  vimls = {},
-  pyright = {},
-  cssls = {},
-  html = {},
-  eslint = {},
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
+	tsserver = {},
+	gopls = {},
+	bashls = {},
+	dockerls = {},
+	vimls = {},
+	pyright = {},
+	cssls = {},
+	html = {},
+	eslint = {},
+	lua_ls = {
+		Lua = {
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
 }
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-local function on_attach()
-end
+local function on_attach() end
 
 -- Setup mason so it can manage external tooling
-require('mason').setup()
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
 
 -- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
+local mason_lspconfig = require("mason-lspconfig")
 
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
+mason_lspconfig.setup({
+	ensure_installed = vim.tbl_keys(servers),
+})
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-}
+mason_lspconfig.setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = servers[server_name],
+		})
+	end,
+})
 
 -- Automatically update diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = false,
-  severity_sort = true,
-  signs = true
+	underline = true,
+	update_in_insert = false,
+	virtual_text = false,
+	severity_sort = true,
+	signs = true,
 })
 
 -- Setup the diagnostics icons
 local function lspSymbol(name, icon)
-  vim.fn.sign_define('DiagnosticSign' .. name, { text = icon, numhl = 'DiagnosticDefault' .. name })
+	vim.fn.sign_define("DiagnosticSign" .. name, { text = icon, numhl = "DiagnosticDefault" .. name })
 end
 
-lspSymbol('Error', '')
-lspSymbol('Warning', '')
-lspSymbol('Information', '')
-lspSymbol('Hint', '')
+lspSymbol("Error", "")
+lspSymbol("Warning", "")
+lspSymbol("Information", "")
+lspSymbol("Hint", "")
 
 -- Configure the look for all floating previews
 -- For more info see: https://github-wiki-see.page/m/neovim/nvim-lspconfig/wiki/UI-customization
 local border = {
-  { "╭", "FloatBorder" }, { "─", "FloatBorder" }, { "╮", "FloatBorder" }, { "│", "FloatBorder" },
-  { "╯", "FloatBorder" }, { "─", "FloatBorder" }, { "╰", "FloatBorder" }, { "│", "FloatBorder" }
+	{ "╭", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "╮", "FloatBorder" },
+	{ "│", "FloatBorder" },
+	{ "╯", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "╰", "FloatBorder" },
+	{ "│", "FloatBorder" },
 }
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-  opts = opts or {}
-  opts.border = opts.border or border
-  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+	opts = opts or {}
+	opts.border = opts.border or border
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
-lspconfig.gopls.setup {
-  cmd = { "gopls", "serve" },
-  filetypes = { "go", "gomod" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      gofumpt = true,
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-      env = {
-        GOFLAGS = "-tags=windows,linux,darwin,integration",
-      },
-    },
-  },
-}
+lspconfig.gopls.setup({
+	cmd = { "gopls", "serve" },
+	filetypes = { "go", "gomod" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			gofumpt = true,
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+			env = {
+				GOFLAGS = "-tags=windows,linux,darwin,integration",
+			},
+		},
+	},
+})
 
-vim.lsp.set_log_level("debug")
+local null_ls = require("null-ls")
+null_ls.setup({
+	sources = {
+		null_ls.builtins.diagnostics.actionlint,
+		null_ls.builtins.diagnostics.codespell,
+		null_ls.builtins.diagnostics.commitlint,
+		null_ls.builtins.diagnostics.gitlint,
+		null_ls.builtins.diagnostics.hadolint,
+		null_ls.builtins.diagnostics.misspell,
+		null_ls.builtins.diagnostics.selene,
+		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.diagnostics.write_good,
+		null_ls.builtins.diagnostics.yamllint,
+		null_ls.builtins.formatting.cbfmt,
+		null_ls.builtins.formatting.jq,
+		null_ls.builtins.formatting.ktlint,
+		null_ls.builtins.formatting.markdownlint,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.shellharden,
+		null_ls.builtins.formatting.stylua,
+	},
+})
+
+-- vim.lsp.set_log_level("debug")
